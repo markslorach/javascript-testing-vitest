2d3b5763df8aa06e183ca592d27545e335a75413
Mark Slorach <markslorach@Marks-MacBook-Pro.local>
2024-07-26 09:30:45 +0100

    tests and function for calculating average

--- src/intro.js
+++ src/intro.js
@@ -1,12 +1,20 @@
 // Lesson: Writing your first tests
 export function max(a, b) {
-  return (a > b) ? a : b;
+  return a > b ? a : b;
 }
 
 // Exercise
 export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
+  if (n % 3 === 0 && n % 5 === 0) return "FizzBuzz";
+  if (n % 3 === 0) return "Fizz";
+  if (n % 5 === 0) return "Buzz";
   return n.toString();
+}
+
+// Calculate average of an array of numbers
+export function calculateAverage(numbers) {
+  if (numbers.length === 0) return NaN;
+
+  const sum = numbers.reduce((total, num) => total + num, 0);
+  return sum / numbers.length;
 }
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { fizzBuzz, max } from "../src/intro";
+import { calculateAverage, fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -18,12 +18,27 @@ describe("fizzBuzz", () => {
     expect(fizzBuzz(15)).toBe("FizzBuzz");
   });
   it("should return 'Fizz' if argument is divisable by 3", () => {
-    expect(fizzBuzz(9)).toBe("Fizz");
+    expect(fizzBuzz(3)).toBe("Fizz");
   });
   it("should return 'Buzz' if argument is divisable by 5", () => {
-    expect(fizzBuzz(10)).toBe("Buzz");
+    expect(fizzBuzz(5)).toBe("Buzz");
   });
   it("should return a string of the number if not valid", () => {
     expect(fizzBuzz(7)).toBe("7");
+  });
+});
+
+describe("calcutlateAverage", () => {
+  it("should return NaN if the array is empty", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+  it("should return the first number if the array has only one number", () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+  it("should return the average number from an array of two numbers", () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+  it("should return the average number from an array of numbers", () => {
+    expect(calculateAverage([1, 2, 3, 4, 5])).toBe(3);
   });
 });
